163c294f4b99e20dd71d6f14940058767d00e4de
이수환 <fidboys1@hubilon.com>
2024-07-04 16:40:21 +0900

    policy 관련 내용 수정내역 적용

--- client/src/components/modal/PrivacyPolicy.tsx
+++ client/src/components/modal/PrivacyPolicy.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import {
     Dialog,
     DialogContent,
@@ -10,10 +10,44 @@ import {
 } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 
-const PrivacyPolicy: React.FC<{ isOpen: boolean; handleOpen: () => void }> = ({
-    isOpen,
-    handleOpen,
-}) => {
+type Props = {
+    isOpen: boolean;
+    handleOpen: () => void;
+};
+
+const termsGenerator = () => {
+    return [
+        {
+            title: 'Last Revised: November 2022', description: ''
+        },
+        {
+            title: '', description: `These Terms and Conditions together with our Privacy Notice govern your
+                        use or viewing of this website (the “Site”) and your relationship with
+                        the owner of the Site, Roche Diagnostics (the “Site Owner”). You may
+                        contact Roche Diagnostics at F. Hoffmann-La Roche Ltd, Corporate
+                        Communications, Grenzacherstrasse 124, CH-4070 Basel, Switzerland. By
+                        using this Site, you agree to be bound by these Terms and Conditions.`
+        },            
+        { title: '1. Terms of Use: ', description: '' },
+        { title: '2. Not Health Advice: ', description: '' },
+        { title: '3. DISCLAIMER OF WARRANTIES: ', description: '' },
+        { title: '4. LIABILITY: ', description: '' },
+        { title: '5. INDEMNITY: ', description: '' },
+        { title: '6. Third Party Content: ', description: '' },
+        { title: '7. Copyright and Trademark: ', description: '' },
+        {
+            title: '8. No Offers or Solicitations; Forward-Looking Statements: ',
+            description: '',
+        },
+    ];
+};
+
+const PrivacyPolicy: React.FC<Props> = ({ isOpen, handleOpen }) => {
+
+    const terms = useMemo(() => {
+        return termsGenerator();
+    }, []);
+
     return (
         <Dialog open={isOpen} onClose={handleOpen}>
             <DialogTitle>
@@ -33,67 +67,14 @@ const PrivacyPolicy: React.FC<{ isOpen: boolean; handleOpen: () => void }> = ({
             </DialogTitle>
             <DialogContent dividers>
                 <List>
-                    <ListItem>
-                        <Typography variant="body1" gutterBottom>
-                            Last Revised: November 2022
-                        </Typography>
-                    </ListItem>
-                    <ListItem>
-                        <Typography variant="body1" gutterBottom>
-                            These Terms and Conditions together with our Privacy Notice govern your
-                            use or viewing of this website (the “Site”) and your relationship with
-                            the owner of the Site, Roche Diagnostics (the “Site Owner”). You may
-                            contact Roche Diagnostics at F. Hoffmann-La Roche Ltd, Corporate
-                            Communications, Grenzacherstrasse 124, CH-4070 Basel, Switzerland. By
-                            using this Site, you agree to be bound by these Terms and Conditions.
-                        </Typography>
-                    </ListItem>
-
-                    <ListItem>
-                        <Typography variant="body1" gutterBottom>
-                            1. Terms of Use:
-                        </Typography>
-                    </ListItem>
-
-                    <ListItem>
-                        <Typography variant="body1" gutterBottom>
-                            2. Not Health Advice:
-                        </Typography>
-                    </ListItem>
-
-                    <ListItem>
-                        <Typography variant="body1" gutterBottom>
-                            3. DISCLAIMER OF WARRANTIES:
-                        </Typography>
-                    </ListItem>
-
-                    <ListItem>
-                        <Typography variant="body1" gutterBottom>
-                            4. LIABILITY:
-                        </Typography>
-                    </ListItem>
-
-                    <ListItem>
-                        <Typography variant="body1" gutterBottom>
-                            5. INDEMNITY:
-                        </Typography>
-                    </ListItem>
-
-                    <ListItem>
-                        <Typography variant="body1" gutterBottom>
-                            6. Third Party Content:
-                        </Typography>
-                    </ListItem>
-                    <ListItem>
-                        <Typography variant="body1" gutterBottom>
-                            7. Copyright and Trademark:
-                        </Typography>
-                    </ListItem>
-                    <ListItem>
-                        <Typography variant="body1" gutterBottom>
-                            8. No Offers or Solicitations; Forward-Looking Statements:
-                        </Typography>
-                    </ListItem>
+                    {terms.map((v) => (
+                        <ListItem>
+                            <Typography variant="body1" gutterBottom>
+                                {v.title}
+                                {v.description}
+                            </Typography>
+                        </ListItem>
+                    ))}
                 </List>
             </DialogContent>
         </Dialog>
