ba51032363f31c07a855127353bf3c0c42a2c0f5
hyojin-hubilon <hyojinan@hubilon.com>
2024-07-18 11:03:12 +0900

    서베이 생성 수정중

--- client/src/pages/survey/components/FormBuilder.tsx
+++ client/src/pages/survey/components/FormBuilder.tsx
@@ -1,54 +1,48 @@
 import { ExampleList, SurveyExample, SurveyQuestion } from "@/types/survey";
-import { AppBar, Box, Button, Card, Container, Divider, FormControlLabel, IconButton, Input, Menu, MenuItem, Radio, RadioGroup, Select, Switch, TextField, Toolbar, Tooltip, useTheme } from "@mui/material";
+import { AppBar, Box, Button, Card, Container, Divider, FormControlLabel, IconButton, Input, List, Menu, MenuItem, Radio, RadioGroup, Select, Switch, TextField, Toolbar, Tooltip, useTheme } from "@mui/material";
 import { useEffect, useState } from "react";
 import ClearIcon from '@mui/icons-material/Clear';
-import ContentCopyIcon from '@mui/icons-material/ContentCopy';
-import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import FormQuestion from "./FormBuilder/FormQuestion";
+import { DragDropContext, Droppable, DropResult } from "@hello-pangea/dnd";
+import { reorder } from "@/utils/helper";
+
 
 
 const FormBuilder = () => {
 	const theme = useTheme();
 	const { primary } = theme.palette;
-	const preQuestion = { question_type: "WRITE", exampleList: [{example_title: '', example_value: 0, sort: 0 }]}
-	const [ question, setQuestion ] = useState<SurveyQuestion>(preQuestion as SurveyQuestion);
+	const preQuestion = { question_type: "WRITE", question:"", exampleList: [{example_title: '', example_value: 0, sort: 0 }]} as SurveyQuestion;
+	const [ questions, setQuestions ] = useState<SurveyQuestion[]>([preQuestion]);
+	
+
+	const addQuestion = () => {
+		setQuestions(prevList => [...prevList, preQuestion]);
+	}
+
+	const onDragEnd = ({ destination, source }: DropResult) => {
+		// dropped outside the list
+		if (!destination || !questions) return;
+	
+		const newItems = reorder(questions, source.index, destination.index);
+
+		newItems
+	
+		setQuestions(newItems);
+	};
+
+	const handleQuestionChanged = (question) => {
+
+	}
+
+	const handleQuetionDeleted = (index) => {
+		
+	}
+	
 
 	useEffect(() => {
-		console.log(question);
-	}, [question])
+		console.log(questions)
+	}, [questions]);
 
-	const handleChangeQuestionType = (e) => {
-		const questionType = e;
-		const newExampleList:SurveyExample = {
-			example_title: '',
-			example_value: 1,
-			sort: 1
-		};
-		setQuestion((prevState) => ({...prevState, question_type : questionType, exampleList: [newExampleList] }))
-	}
-
-	const handleAddExample = () => {
-		const exampleListLength = question.exampleList.length + 1;
-		const newExample:SurveyExample = {
-			example_title: '',
-			example_value: exampleListLength,
-			sort: exampleListLength
-		};
-
-		setQuestion((prevState) => ({...prevState, exampleList: [...prevState.exampleList, newExample] }))
-	}
-
-	const handleDeleteExample = (sort) => {
-		const newExample = question.exampleList.filter(example => example.sort !== sort);
-		newExample.map((example, index) => {
-			example.sort = index+1;
-			example.example_value = index + 1
-		});
-		setQuestion((prevState) => ({...prevState, exampleList: [...newExample] }))
-	}
-
-	const handleAddEtc = () => {
-
-	}
 
 	return (
 		<Box display="flex" flexDirection="column" gap={1}>
@@ -59,94 +53,20 @@ const FormBuilder = () => {
 					<TextField id="description" placeholder="설문지 설명" variant="standard" fullWidth />
 				</Box>
 			</Card>
-			{/* 질문 영역 */}
-			<Card sx={{width: '100%', p: 3}}>
-				<Box display="flex" flexDirection="column" gap={1}>
-					<Box display="flex" gap={1}>
-						<TextField defaultValue="제목없는 질문" placeholder="질문" variant="standard"  />
-						<Select 
-							value={question.question_type}
-							onChange={(e) => handleChangeQuestionType(e.target.value)}
-						>
-							<MenuItem value="WRITE">주관식 답변</MenuItem>
-							<MenuItem value="SINGLE">객관식 답변(단일응답)</MenuItem>
-							<MenuItem value="MULTIPLE">객관식 답변(복수응답)</MenuItem>
-						</Select>
-					</Box>
-					{
-						question.question_type == "WRITE"
-						?
-						<TextField placeholder="주관식 답변" variant="standard" fullWidth inputProps={{ readOnly: true }} />
-						:
-						<Box>
-							{
-								question.exampleList && question.exampleList.map((example, index) => 
-									{
-										return (
-											<Box key={index + 1}>
-												<Radio readOnly disabled />
-												<TextField placeholder={`옵션 ${index + 1}`} variant="standard" />
-												{
-													question.exampleList && question.exampleList.length > 1 &&
-													<IconButton onClick={() => handleDeleteExample(example.sort)}>
-														<ClearIcon />
-													</IconButton>
-												}
-												
-											</Box>
-										)
-									}
-									
-								)
-							}
-							
-							{/* 옵션추가영역 */}
-							<Box>
-								<Radio readOnly disabled />
-								<TextField placeholder="옵션추가"
-									variant="standard"
-									inputProps={{readOnly: true}}
-									onClick={handleAddExample} /> 
-								<span>또는</span>
-								<Button onClick={handleAddEtc}>기타추가</Button> 
-								{/* 기타는 하나를 추가하면 기타추가 버튼이 사라져야 함, 기타는 무조건 맨 밑에 위치해야함 */}
-							</Box>
-
-						</Box>
-
-					}
-					
-					<Divider />
-					<Box display="flex">
-						{/* 복사 */}
-						<Tooltip title="질문 복사">
-							<IconButton>
-								<ContentCopyIcon />
-							</IconButton>
-						</Tooltip>
-						
-
-						{/* 삭제 */}
-						<Tooltip title="질문 삭제">
-							<IconButton>
-								<DeleteOutlineIcon />
-							</IconButton>
-						</Tooltip>
-
-						{/* 세로 디바이더 */}
-						<Divider flexItem /> 
-						필수
-						<Switch
-							// checked={checked}
-							// onChange={handleChange}
-							inputProps={{ 'aria-label': 'controlled' }}
-							/>
-					</Box>
-				</Box>
-			</Card>
-			{/* 질문 영역 끝 */}
-
-			<Button>질문 추가</Button>
+			<DragDropContext onDragEnd={onDragEnd}>
+				<Droppable droppableId="question-droppable-list">
+					{(provided) => (
+						<div ref={provided.innerRef} {...provided.droppableProps}>
+							{questions.map((question: SurveyQuestion, index: number) => (
+								<FormQuestion oneQuestion={question} key={index} index={index} questionChanged={(question) => handleQuestionChanged(question)} questionDeleted={(index) => handleQuetionDeleted(index)} />
+							))}
+							{provided.placeholder}
+						</div>
+					)}
+				</Droppable>
+			</DragDropContext>
+			
+			<Button onClick={addQuestion}>질문 추가</Button>
 		</Box>
 	)
 }
--- client/src/pages/survey/components/FormBuilder/FormQuestion.tsx
+++ client/src/pages/survey/components/FormBuilder/FormQuestion.tsx
@@ -1,4 +1,166 @@
-const FormQuestion = () => {
+import { SurveyExample, SurveyQuestion } from "@/types/survey";
+import { Card, Box, TextField, Select, MenuItem, Radio, IconButton, Divider, Tooltip, Switch, useTheme } from "@mui/material";
+import { ClearIcon } from "@mui/x-date-pickers";
+import { useEffect, useState } from "react";
+
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import { Draggable } from "@hello-pangea/dnd";
+
+
+type FormQuestionProps = {
+	oneQuestion: SurveyQuestion,
+	index: number,
+	questionChanged: (question:SurveyQuestion) => void,
+	questionDeleted: (index: number) => void
+}
+
+const FormQuestion = ({ oneQuestion, index, questionChanged, questionDeleted }:FormQuestionProps) => {
+	const theme = useTheme();
+	const { grey, primary } = theme.palette;
+	const [ question, setQuestion ] = useState(oneQuestion);
 	
+	useEffect(() => {
+		console.log(question);
+	}, [question])
+
+	const handleChangeQuestionType = (e) => {
+		const questionType = e;
+		const newExampleList:SurveyExample = {
+			example_title: '',
+			example_value: 1,
+			sort: 1
+		};
+		setQuestion((prevState) => ({...prevState, question_type : questionType, exampleList: [newExampleList] }))
+	}
+
+	const handleAddExample = () => {
+		const exampleListLength = question.exampleList.length + 1;
+		const newExample:SurveyExample = {
+			example_title: '',
+			example_value: exampleListLength,
+			sort: exampleListLength
+		};
+
+		setQuestion((prevState) => ({...prevState, exampleList: [...prevState.exampleList, newExample] }))
+	}
+
+	const handleDeleteExample = (sort) => {
+		const newExample = question.exampleList.filter(example => example.sort !== sort);
+		newExample.map((example, index) => {
+			example.sort = index+1;
+			example.example_value = index + 1
+		});
+		setQuestion((prevState) => ({...prevState, exampleList: [...newExample] }))
+	}
+
+	const handleAddEtc = () => {
+
+	}
+
+	
+	return (
+		<Draggable draggableId={`question-${index}`} index={index}>
+			{(provided, snapshot) => (
+				<Card 
+					ref={provided.innerRef}
+					{...provided.draggableProps}
+					{...provided.dragHandleProps}
+					sx={{
+						p: 3,
+						background : snapshot.isDragging ? 'rgb(235,235,235)' : 'white',
+						borderLeft: snapshot.isDragging ? `5px solid ${primary.main}` : '',
+						borderRadius: "5px", cursor:'pointer',
+						mb: '5px',
+						'&:last-child': {
+							mb: 0
+						}
+					}}>
+					<Box display="flex" flexDirection="column" gap={1}>
+						<Box display="flex" gap={1}>
+							<TextField defaultValue="제목없는 질문" placeholder="질문" variant="standard"  value={question.question} />
+							<Select 
+								value={question.question_type}
+								onChange={(e) => handleChangeQuestionType(e.target.value)}
+							>
+								<MenuItem value="WRITE">주관식 답변</MenuItem>
+								<MenuItem value="SINGLE">객관식 답변(단일응답)</MenuItem>
+								<MenuItem value="MULTIPLE">객관식 답변(복수응답)</MenuItem>
+							</Select>
+						</Box>
+						{
+							question.question_type == "WRITE"
+							?
+							<TextField placeholder="주관식 답변" variant="standard" fullWidth inputProps={{ readOnly: true }} />
+							:
+							<Box>
+								{
+									question.exampleList && question.exampleList.map((example, index) => 
+										{
+											return (
+												<Box key={index + 1}>
+													<Radio readOnly disabled />
+													<TextField placeholder={`옵션 ${index + 1}`} variant="standard" />
+													{
+														question.exampleList && question.exampleList.length > 1 &&
+														<IconButton onClick={() => handleDeleteExample(example.sort)}>
+															<ClearIcon />
+														</IconButton>
+													}
+													
+												</Box>
+											)
+										}
+										
+									)
+								}
+								
+								{/* 옵션추가영역 */}
+								<Box>
+									<Radio readOnly disabled />
+									<TextField placeholder="옵션추가"
+										variant="standard"
+										inputProps={{readOnly: true}}
+										onClick={handleAddExample} /> 
+									{/* <span>또는</span>
+									<Button onClick={handleAddEtc}>기타추가</Button>  */}
+									{/* 기타는 하나를 추가하면 기타추가 버튼이 사라져야 함, 기타는 무조건 맨 밑에 위치해야함 */}
+								</Box>
+
+							</Box>
+
+						}
+						
+						<Divider />
+						<Box display="flex">
+							{/* 복사 */}
+							<Tooltip title="질문 복사">
+								<IconButton>
+									<ContentCopyIcon />
+								</IconButton>
+							</Tooltip>
+							
+
+							{/* 삭제 */}
+							<Tooltip title="질문 삭제">
+								<IconButton onClick={() => questionDeleted(index)}>
+									<DeleteOutlineIcon />
+								</IconButton>
+							</Tooltip>
+
+							{/* 세로 디바이더 */}
+							<Divider flexItem /> 
+							필수
+							<Switch
+								// checked={checked}
+								// onChange={handleChange}
+								inputProps={{ 'aria-label': 'controlled' }}
+								/>
+						</Box>
+					</Box>
+				</Card>
+			)}
+		</Draggable>
+	)
 }
 export default FormQuestion;
