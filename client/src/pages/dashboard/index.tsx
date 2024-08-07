51420e1258598248c6938337e59f03790daccac7
hyojin-hubilon <hyojinan@hubilon.com>
2024-08-07 18:01:02 +0900

    dashboard index 내용 추가중

--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -1,67 +1,95 @@
 import { EditOutlined, SyncOutlined } from "@ant-design/icons";
-import { Box, Button, Grid, Typography } from "@mui/material";
+import { Box, Button, Container, Grid, styled, Typography } from "@mui/material";
 import MainCard from "@/components/MainCard";
 import CircleChart, { ApexDonutChartSeriesType } from "../study/components/overview/CircleChart";
 import dashboardApi from "@/apis/dashboard";
 import { useEffect, useState } from "react";
+import dayjs from "dayjs";
+import { NumOfParticipantByStudy } from "@/types/dashboard";
 
+export const GreyBox = styled(Box)(({theme}) => ({
+	backgroundColor: theme.palette.grey[50],
+	borderRadius: '1rem',
+	padding: '1rem',
+	height: '100%',
+	border: `1px solid ${theme.palette.grey[200]}`
+}))
 
+export const H5LengthSixteen = styled(Typography)`
+	max-width: 240px;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+`;
 
 const DashboardDefault = () => {
-	const [ participantNumber, setParticipantNumber] = useState<any>();
+	const [ participantNumber, setParticipantNumber] = useState<NumOfParticipantByStudy[]>([]);
+	const [ loadedTime, setLoadedTime ] = useState(new Date());
 	const getNumberParticipant = async () => {
 		const response = await dashboardApi.getNumOfParticipantByStudy();
 		setParticipantNumber(response.content);
+
+		setLoadedTime(new Date())
 	}
-
-
-	const partCompleteRate: ApexDonutChartSeriesType = {
-        labels: ['참여완료율', '미완료율'],
-        series: [75, 25],
-    };
+	
+	const handleRefresh =() => {
+		getNumberParticipant();
+	}
 
 
 	useEffect(() => {
 		getNumberParticipant();
 	}, [])
 
+	const getPartCompleteRate = (studyNum:NumOfParticipantByStudy) => {
+		const targetNumber = studyNum.target_number;
+		
+		return {
+			labels: ['참여완료율', '미완료율'], //미완료 컬러변경 필요
+			series: [studyNum.number_participant, (targetNumber - studyNum.number_participant)],
+		};
+	}
+
 	
     return  (
 		<>
-            {/* <Grid container rowSpacing={3} columnSpacing={1}>
-                <Grid container item xs={12}>
-                    <Box display="flex" alignItems="center" gap={1}>
-                        <Typography variant="h3">Dashborad</Typography>
+            <Grid item rowSpacing={3} columnSpacing={1}>
+                <Grid container item xs={12} justifyContent="space-between" alignItems="center" mb={2}>
+                    <Box>
+                        <Typography variant="h3">Dashboard</Typography>
                     </Box>
 					<Box>
-						<Button>2024-07-19 12:34:05 <SyncOutlined /></Button>
+						<Button onClick={handleRefresh}>{ dayjs(loadedTime).format('YYYY-MM-DD hh:mm:ss')} <SyncOutlined style={{marginLeft: '0.5rem'}} /></Button>
 					</Box>
                 </Grid>
 
-				<Grid container item columnSpacing={1.5}>
-				Number of participants by study
-				<Grid item xs={2}>
-					<MainCard sx={{height: '190px'}} overflow="visible">
-						<Typography variant="h6" color="textSecondary">참여완료율</Typography>
-						<CircleChart series={partCompleteRate} />
-					</MainCard>
-				</Grid>
-				<Grid item xs={2}>
-					<MainCard sx={{height: '190px'}}>
-						<Typography variant="h6" color="textSecondary">
-							참여자수
-						</Typography>
-						<Box>
-							<Typography variant="h2" color="primary" sx={{display: 'block', textAlign: 'center', mt: '2rem'}}>1500</Typography>
-							<Typography variant="h5" sx={{display: 'block', textAlign: 'center', ml: '2rem'}}>/2000</Typography>
-						</Box>
-					</MainCard>
-				</Grid>
-			</Grid>
+				<Grid item columnSpacing={1.5} xs={12}>
+					
+					<MainCard sx={{width: 1}}>
+						<Typography variant="h4" mb={2}>Number of participants by study</Typography>
+						<Grid item container xs={12} gap={2}>
+							{
+								participantNumber.map(study => 
+									<Grid item xs={3.9} sx={{maxWidth: '300px !important'}}>
+										<GreyBox>
+											<H5LengthSixteen variant="h5">{study.title}</H5LengthSixteen>
+											<Box display="flex" justifyContent="center">
+												<Box display="flex" alignItems="center">
+													<Typography variant="h3" color="primary">{ study.number_participant }</Typography>
+													<Typography variant="h5" sx={{ml:'0.5rem'}}> / { study.target_number} </Typography>
+												</Box>
 
-                
-               
-            </Grid> */}
+												<CircleChart series={getPartCompleteRate(study)} />
+											</Box>
+										</GreyBox>
+									</Grid>
+								)
+							}
+						</Grid>
+					</MainCard>
+
+				</Grid>               
+            </Grid>
         </>
 	);
 };
