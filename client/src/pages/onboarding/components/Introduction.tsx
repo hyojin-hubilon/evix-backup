d306712826e292e41a3d2742eba83861c150c3bc
hyojin-hubilon <hyojinan@hubilon.com>
2024-09-12 13:38:29 +0900

    온보딩 페이지 컨텐츠 추가

--- client/src/pages/onboarding/components/Introduction.tsx
+++ client/src/pages/onboarding/components/Introduction.tsx
@@ -1,22 +1,35 @@
 import { Box, Button, Grid, Typography } from "@mui/material";
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import logoWhite from '@assets/images/onboarding/logo_white.svg'
-import onboardingImg01 from '@assets/images/onboarding/01.jpg'
-const Introduction = () => {
+import { OnboardingContentsType } from "..";
+import { Trans } from "react-i18next";
+
+type IntroductionType = {
+	contents: OnboardingContentsType;
+	next:() => void,
+	activeIndex: number,
+	contentsLength: number
+}
+
+const Introduction = ({contents, next, activeIndex, contentsLength} : IntroductionType) => {
 	return (
 		<Box component="section" display="flex" minWidth="100vw" minHeight="100vh" bgcolor="#F9FAFB">
 			<Box width="50%" position="relative">
-				<Box ml="20%" mt="20%">
+				<Box ml="20%" mt="15%" mr="20%">
 					<Typography variant="h1"
 						sx={{
 							fontSize: '3rem',
 							fontWeight: 700,
 							letterSpacing: '.02em',
-							mt:'2rm',
-							mb: '3rem'
+							mt:'1rm',
+							mb: '3rem',
+							'strong' : {
+								color: '#2D76EE',
+								fontWeight:700
+							}
 						}}				
 					>
-						Welcome to evix-DCT
+						<Trans i18nKey={contents.title} components={{ strong: <strong /> }} />
 					</Typography>
 					<Box display="flex" flexDirection="column" gap={2}
 						sx={{
@@ -26,34 +39,35 @@ const Introduction = () => {
 								letterSpacing: '0.02em',
 							}
 						}}>
-						<Typography display="flex" alignItems="center">
-							<CheckCircleIcon sx={{mr: '1rem'}}/> <span>Create Study</span>
-						</Typography>
-
-						<Typography display="flex" alignItems="center">
-							<CheckCircleIcon sx={{mr: '1rem'}}/> <span>Open Study</span>
-						</Typography>
-
-						<Typography display="flex" alignItems="center">
-							<CheckCircleIcon sx={{mr: '1rem'}}/> <span>Real-time monitoring</span>
-						</Typography>
-
-						<Typography display="flex" alignItems="center">
-							<CheckCircleIcon sx={{mr: '1rem'}}/> <span>Report results</span>
-						</Typography>
+							{
+								contents.sub.map((subText, index) => 
+								<Typography display="flex" alignItems="flex-start" key={index}>
+									<CheckCircleIcon sx={{mt:'0.4rem', mr: '1rem'}}/> <span>{subText}</span>
+								</Typography>
+								)
+							}
 					</Box>
 				</Box>
 				<Grid container position="absolute" bottom="0" left="0" width="100%" p="1rem" justifyContent="space-between" borderTop="1px solid #E4E7EC;">
-					<Grid item xs={5.9}>
-						<Button variant="outlined" fullWidth>Skip</Button>
-					</Grid>	
-					<Grid item xs={5.9}>
-						<Button variant="contained" fullWidth color="primary">Next</Button>
-					</Grid>
+					{
+						activeIndex == contentsLength - 1 ? 
+						<Grid item xs={5.9}>
+							<Button variant="contained" fullWidth color="primary" onClick={next} size="large">시작하기</Button>
+						</Grid>:
+						<>
+							<Grid item xs={5.9}>
+								<Button variant="outlined" fullWidth size="large">Skip</Button>
+							</Grid>	
+							<Grid item xs={5.9}>
+								<Button variant="contained" fullWidth color="primary" onClick={next} size="large">Next</Button>
+							</Grid>
+						</>
+					}
+					
 				</Grid>
 			</Box>
 			<Box width="50%" sx={{
-				background : `url(${onboardingImg01}) no-repeat center center`,
+				background : `url(${contents.imgUrl}) no-repeat center center`,
 				backgroundSize: 'cover'
 				}}>
 				<Box sx={{
@@ -70,7 +84,7 @@ const Introduction = () => {
 							fontSize: '1rem',
 							fontWeight: 500,
 							letterSpacing: '0.5px',
-						}}>1/5</Typography>
+						}}>{activeIndex + 1}/5</Typography>
 					</Box>
 				</Box>
 			</Box>
--- client/src/pages/onboarding/index.tsx
+++ client/src/pages/onboarding/index.tsx
@@ -1,17 +1,85 @@
 
+import { t } from "i18next";
 import Introduction from "./components/Introduction";
+import onboardingImg01 from '@assets/images/onboarding/01.jpg';
+import onboardingImg02 from '@assets/images/onboarding/02.jpg';
+import onboardingImg03 from '@assets/images/onboarding/03.jpg';
+import onboardingImg04 from '@assets/images/onboarding/04.jpg';
+import onboardingImg05 from '@assets/images/onboarding/05.jpg';
+import { useState } from "react";
+
+export type OnboardingContentsType = {
+	title: string;
+	sub: string[];
+	imgUrl:string;
+}
 
 const Onboarding = () => {
-	const onboardingContents = [
+	const [activeIndex, setActiveIndex] = useState(1);
+	const onboardingContents: OnboardingContentsType[] = [
 		{
-			title: '',
-			sub: [],
-			imgIrl: ''
+			title: t('onboarding.welcome_to_evix-dct'),
+			sub: [
+				t('onboarding.create_study'),
+				t('onboarding.open_study'),
+				t('onboarding.real-time_monitoring'),
+				t('onboarding.report_results')
+			],
+			imgUrl: onboardingImg01
+		},
+		{
+			title: t('onboarding.create_surveys'),
+			sub: [
+				t('onboarding.we_provide_survey'),
+				t('onboarding.we_provide_tools'),
+				t('onboarding.see_patient_engagement'),
+				t('onboarding.the_collected_data'),
+			],
+			imgUrl: onboardingImg02
+		},
+		{
+			title: t('onboarding.increase_engagement'),
+			sub: [
+				t('onboarding.surveys_that_require'),
+				t('onboarding.notifications_will_be_sent'),
+				t('onboarding.participants_participate'),
+			],
+			imgUrl: onboardingImg03
+		},
+		{
+			title: t('onboarding.invite_administrators'),
+			sub: [
+				t('onboarding.invite_managers'),
+				t('onboarding.provides_effective')
+			],
+			imgUrl: onboardingImg04
+		},
+		{
+			title: t('onboarding.provides_real-time'),
+			sub: [
+				t('onboarding.check_the_status'),
+				t('onboarding.real-time_statistics')
+			],
+			imgUrl: onboardingImg05
 		}
 	]
+	const handleClickNext = () => {
+
+	}
 
 	return (
-		<Introduction />
+		<>
+			{
+				onboardingContents.map((contents, index) => 
+					<>
+					{
+						activeIndex == index && <Introduction contents={contents} next={handleClickNext} key={index} activeIndex={activeIndex} contentsLength={onboardingContents.length}/>
+					}
+					</>
+				)
+			}
+		</>
+		
 	)
 }
 
