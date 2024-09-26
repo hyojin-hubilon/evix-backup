3751a79f7870ba4580431e2a54de73fc5e651f86
hyojin-hubilon <hyojinan@hubilon.com>
2024-09-26 14:26:18 +0900

    Apply free trial country 추가, Industry select box로 변경

--- client/src/pages/landing/Support.tsx
+++ client/src/pages/landing/Support.tsx
@@ -4,7 +4,7 @@ import MainCard from '@components/MainCard';
 const SupportPage = () => {
     return (
         <Grid container alignItems="center" justifyContent="center" width={1}>
-            <Grid item xs={12} sm={8} md={8} lg={6}>
+            <Grid item xs={12} sm={10} md={8} lg={6}>
                 <MainCard
                     sx={{
                         margin: { xs: 2.5, md: 3 }, // 마진을 설정합니다.
